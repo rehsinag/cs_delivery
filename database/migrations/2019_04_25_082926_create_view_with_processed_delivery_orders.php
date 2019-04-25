1992bf54d3bd9db71f7f6b53c57c92a36d2c272c
G <gabdullayev@crystalspring.kz>
2019-04-25 14:35:09 +0600

    Представление для обработанных заказов

---
+++ database/migrations/2019_04_25_082926_create_view_with_processed_delivery_orders.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewWithProcessedDeliveryOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('CREATE VIEW `deliveryOrdersProcessed` AS
+            select * from `deliveryOrders` where `status` = "' . \App\Status::COMPLETED .'"
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP VIEW `deliveryOrdersProcessed`');
+    }
+}
