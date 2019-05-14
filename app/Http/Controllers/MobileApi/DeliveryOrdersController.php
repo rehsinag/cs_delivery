528e5a72c3fd65e852d56f46f7f44f3b927fdd57
G <gabdullayev@crystalspring.kz>
2019-05-14 12:10:09 +0600

    test

--- app/Http/Controllers/MobileApi/DeliveryOrdersController.php
+++ app/Http/Controllers/MobileApi/DeliveryOrdersController.php
@@ -187,6 +187,7 @@ class DeliveryOrdersController extends Controller
         {
             $deliveryUserComments = DeliveryUserComment::getCollection(['requestIdIn' => $requestId, 'orderBy' => 'created_at'])->get();
 var_dump($deliveryUserComments);
+die;
             if(count($deliveryUserComments))
             {
                 $output = [];
