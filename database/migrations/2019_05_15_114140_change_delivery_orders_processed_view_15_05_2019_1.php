bb7869f37e829ef9f6d1e16fec911c690010c8cd
G <gabdullayev@crystalspring.kz>
2019-05-15 18:27:01 +0600

    изменение представления deliveryOrdersProcessed. поля cFIO, cComment

---
+++ database/migrations/2019_05_15_114140_change_delivery_orders_processed_view_15_05_2019_1.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDeliveryOrdersProcessedView150520191 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('ALTER VIEW `deliveryOrdersProcessed` AS
+        SELECT *, 
+        (SELECT CONCAT(`firstName`, \' \', `lastName`, \' \', `middleName`) FROM `deliveryusers` u WHERE u.id=deliveryUserId) As `cFIO`, 
+        (SELECT `text` FROM `deliveryuserscomments` c WHERE c.requestId=`o`.requestId ORDER BY `created_at` DESC LIMIT 1) AS `cComment` 
+        FROM `deliveryorders` as `o` 
+        WHERE `status` IN ("' . \App\Status::DELIVERED .'", "' . \App\Status::REJECT_PRODUCT .'", "' . \App\Status::REJECT_DELIVERY .'", "' . \App\Status::REJECT_VISUAL .'", "' . \App\Status::NOT_DIAL .'") 
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('ALTER VIEW `deliveryOrdersProcessed` AS
+        select * from `deliveryOrders` where `status` IN ("' . \App\Status::DELIVERED .'", "' . \App\Status::REJECT_PRODUCT .'", "' . \App\Status::REJECT_DELIVERY .'")
+        ');
+    }
+}
